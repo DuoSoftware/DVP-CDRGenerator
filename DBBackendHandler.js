cea86fd664ef0d9e50cba5f2b18bef10938a77d7
dinusha_dck <dinusha@duosoftware.com>
2017-05-31 17:05:42 +0530

    fix typo OUTBOUND_DENIED

--- DBBackendHandler.js
+++ DBBackendHandler.js
@@ -20,7 +20,7 @@ var getCallLeg = function(uuid, callback)
 {
     try
     {
-        dbModel.CallCDR.find({where :[{Uuid: uuid, Direction: 'inbound', ObjCategory: {ne: 'CONFERENCE'}, $or: [{OriginatedLegs: {ne: null}}, {OriginatedLegs: null, $or:[{ObjType: 'HTTAPI'},{ObjType: 'SOCKET'},{ObjType: 'REJECTED'},{ObjCategory: 'DND'},{ObjCategory: 'OUTBOUND DENIED'}]}]}]}).then(function(callLeg)
+        dbModel.CallCDR.find({where :[{Uuid: uuid, Direction: 'inbound', ObjCategory: {ne: 'CONFERENCE'}, $or: [{OriginatedLegs: {ne: null}}, {OriginatedLegs: null, $or:[{ObjType: 'HTTAPI'},{ObjType: 'SOCKET'},{ObjType: 'REJECTED'},{ObjCategory: 'DND'},{ObjCategory: 'OUTBOUND_DENIED'}]}]}]}).then(function(callLeg)
         {
             callback(undefined, callLeg);
 
