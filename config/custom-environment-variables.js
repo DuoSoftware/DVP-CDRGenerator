23709389096e53f6df6c921dfa13c8c79e93ff4c
sukitha <sukitha@duosoftware.com>
2017-06-29 12:10:41 +0530

    redis sentinel support added

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -13,20 +13,35 @@ module.exports = {
         "Database":"SYS_DATABASE_POSTGRES_USER"
     },
 
-    "Security":
+    "Redis":
     {
+        "mode":"SYS_REDIS_MODE",
         "ip": "SYS_REDIS_HOST",
         "port": "SYS_REDIS_PORT",
         "user": "SYS_REDIS_USER",
-        "password": "SYS_REDIS_PASSWORD"
+        "password": "SYS_REDIS_PASSWORD",
+        "sentinels":{
+            "hosts": "SYS_REDIS_SENTINEL_HOSTS",
+            "port":"SYS_REDIS_SENTINEL_PORT",
+            "name":"SYS_REDIS_SENTINEL_NAME"
+        }
 
     },
-    "Redis":
+
+    "Security":
     {
+
         "ip": "SYS_REDIS_HOST",
         "port": "SYS_REDIS_PORT",
+        "user": "SYS_REDIS_USER",
         "password": "SYS_REDIS_PASSWORD",
-        "db": "SYS_REDIS_DB_PROCESSEDCDR"
-    }
+        "mode":"SYS_REDIS_MODE",
+        "sentinels":{
+            "hosts": "SYS_REDIS_SENTINEL_HOSTS",
+            "port":"SYS_REDIS_SENTINEL_PORT",
+            "name":"SYS_REDIS_SENTINEL_NAME"
+        }
+
+    },
 
 };
